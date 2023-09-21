b71a8e8056262d89067afb4523dcc3a06d7a56ae
alex <alex.perraud@orange.com>
2023-09-21 16:28:25 +0200

    ajout de devise

---
+++ tp_1/Models/Devise.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace tp_1.Models
+{
+	public class Devise
+	{
+
+		public int Id
+		{
+			get
+				{
+					return Id;
+				}
+			set
+				{
+					Id = value;
+				}
+		}
+		private string? nomDevise;
+
+        public string? NomDevise
+		{
+			get
+				{
+					return nomDevise;
+				}
+			set
+				{
+					nomDevise = value;
+				}
+		}
+
+		private double taux;
+
+        public Devise(int id, string? nomDevise, double taux)
+        {
+            Id = id;
+            NomDevise = nomDevise;
+            Taux = taux;
+        }
+
+        public double Taux
+		{
+            get
+				{
+					return taux;
+				}
+            set
+				{
+					taux = value;
+				}
+        }
+
+    }
+}
+

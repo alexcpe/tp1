73576e8b6806b7ccbfe5e4fe92ce5866f9165dea
alex <alex.perraud@orange.com>
2023-09-21 17:07:53 +0200

    API GETALL DEVISES AND API GET DEVISE/ID

--- tp_1/Controllers/DevisesController.cs
+++ tp_1/Controllers/DevisesController.cs
@@ -1,47 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using tp_1.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace tp_1.Controllers
+namespace tp_1.Controllers;
+
+[Route("api/[controller]")]
+public class DevisesController : Controller
 {
-    [Route("api/[controller]")]
-    public class DevisesController : Controller
+    private List<Devise> _devises { get; } = new()
     {
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        new Devise(1, "Dollar", 1.08),
+        new Devise(2, "Franc Suisse", 1.07),
+        new Devise(3, "Yen", 120)
+    };
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+    // GET: api/values
+    [HttpGet]
+    public IEnumerable<Devise> GetAll()
+    {
+        return _devises;
+    }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
 
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+    [HttpGet("{id}", Name = "GetDevise")]
+    public ActionResult<Devise> GetById(int id)
+    {
+        var devise = _devises.FirstOrDefault(d => d.Id == id);
+        if (devise == null) return NotFound();
+        return devise;
+    }
 
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+    // POST api/values
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/values/5
+    [HttpPut("{id}")]   
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/values/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
     }
 }
-
--- tp_1/Models/Devise.cs
+++ tp_1/Models/Devise.cs
@@ -1,55 +1,17 @@
-﻿using System;
-namespace tp_1.Models
+﻿namespace tp_1.Models;
+
+public class Devise
 {
-	public class Devise
-	{
-		private int id;
-		public int Id
-		{
-			get
-				{
-					return id;
-				}
-			set
-				{
-					id = value;
-				}
-		}
-		private string? nomDevise;
-
-        public string? NomDevise
-		{
-			get
-				{
-					return nomDevise;
-				}
-			set
-				{
-					nomDevise = value;
-				}
-		}
-
-		private double taux;
-
-        public Devise(int id, string? nomDevise, double taux)
-        {
-            id = id;
-            NomDevise = nomDevise;
-            Taux = taux;
-        }
-
-        public double Taux
-		{
-            get
-				{
-					return taux;
-				}
-            set
-				{
-					taux = value;
-				}
-        }
-
+    public Devise(int id, string? nomDevise, double taux)
+    {
+        Id = id;
+        NomDevise = nomDevise;
+        Taux = taux;
     }
-}
 
+    public int Id { get; set; }
+
+    public string? NomDevise { get; set; }
+
+    public double Taux { get; set; }
+}
